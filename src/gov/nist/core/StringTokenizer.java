AlerantAppNGIN/jain-sip

commit 2a8aa8a162713361ef862afe0515de90392dfbe4

Introduced simple code optimizations.
git-svn-id: https://svn.java.net/svn/jsip~svn/trunk@801 8e71dc83-d81e-6649-80f2-80b843a9b2be

src/gov/nist/core/StringTokenizer.java
@@ -53,22 +53,19 @@ public class StringTokenizer {
 		bufferLen = buffer.length();
 		ptr = 0;
 	}
-
+	
 	public String nextToken() {
-		StringBuffer retval = new StringBuffer();
-
+		int startIdx = ptr;
+		
 		while (ptr < bufferLen) {
-			if (buffer.charAt(ptr) == '\n') {
-				retval.append(buffer.charAt(ptr));
-				ptr++;
+			char c = buffer.charAt(ptr);
+			ptr++;
+			if (c == '\n') {
 				break;
-			} else {
-				retval.append(buffer.charAt(ptr));
-				ptr++;
 			}
 		}
 
-		return retval.toString();
+		return buffer.substring(startIdx, ptr);
 	}
 
 	public boolean hasMoreChars() {
@@ -76,17 +73,9 @@ public class StringTokenizer {
 	}
 
 	public static boolean isHexDigit(char ch) {
-		if (isDigit(ch))
-			return true;
-		else {
-			char ch1 = Character.toUpperCase(ch);
-			return ch1 == 'A'
-				|| ch1 == 'B'
-				|| ch1 == 'C'
-				|| ch1 == 'D'
-				|| ch1 == 'E'
-				|| ch1 == 'F';
-		}
+		return (ch >= 'A' && ch <= 'F') ||
+			   (ch >= 'a' && ch <= 'f') ||
+			   isDigit(ch);
 	}
 
 	public static boolean isAlpha(char ch) {
@@ -102,16 +91,14 @@ public class StringTokenizer {
 	}
 
 	public String getLine() {
-		StringBuffer retval = new StringBuffer();
+		int startIdx = ptr;
 		while (ptr < bufferLen && buffer.charAt(ptr) != '\n') {
-			retval.append(buffer.charAt(ptr));
 			ptr++;
 		}
 		if (ptr < bufferLen && buffer.charAt(ptr) == '\n') {
-			retval.append('\n');
 			ptr++;
-		}
-		return retval.toString();
+		}		
+		return buffer.substring(startIdx, ptr);
 	}
 
 	public String peekLine() {
@@ -127,10 +114,12 @@ public class StringTokenizer {
 
 	public char lookAhead(int k) throws ParseException {
 		// Debug.out.println("ptr = " + ptr);
-		if (ptr + k < bufferLen)
+		try {
 			return buffer.charAt(ptr + k);
-		else
+		}
+		catch (IndexOutOfBoundsException e) {
 			return '\0';
+		}
 	}
 
 	public char getNextChar() throws ParseException {
@@ -164,20 +153,16 @@ public class StringTokenizer {
 	/** Get the next token from the buffer.
 	*/
 	public String getNextToken(char delim) throws ParseException {
-		StringBuffer retval = new StringBuffer();
+		int startIdx = ptr;
 		while (true) {
 			char la = lookAhead(0);
-			
-			//System.out.println("la = " + la);
-			
 			if (la == delim)
 				break;
 			else if (la == '\0')
 				throw new ParseException("EOL reached", 0);
-			retval.append(buffer.charAt(ptr));
 			consume(1);
 		}
-		return retval.toString();
+		return buffer.substring(startIdx, ptr);
 	}
 
 	/** get the SDP field name of the line

src/gov/nist/core/HostNameParser.java
@@ -174,9 +174,10 @@ public class HostNameParser extends ParserCore {
 			hp.setHost(host);
 			// Has a port?
 			if (allowWS) lexer.SPorHT(); // white space before ":port" should be accepted
-			if (lexer.hasMoreChars()) {				
-				switch (lexer.lookAhead(0))
-				{ 
+			if (lexer.hasMoreChars()) {
+				char la = lexer.lookAhead(0);
+				switch (la)
+				{
 				case ':':
 					lexer.consume(1);
 					if (allowWS) lexer.SPorHT(); // white space before port number should be accepted
@@ -189,7 +190,7 @@ public class HostNameParser extends ParserCore {
 							lexer.getPtr());
 					}
 					break;
-				
+
 				case ';':	// OK, can appear in URIs (parameters)
 				case '?':	// same, header parameters
 				case '>':	// OK, can appear in headers
@@ -198,11 +199,11 @@ public class HostNameParser extends ParserCore {
 				case '\r':
 				case '\n':
 					break;
-					
+
 				default:
 					if (!allowWS) {
-						throw new ParseException( lexer.getBuffer() + 
-								" Illegal character in hostname:" + lexer.lookAhead(0), 
+						throw new ParseException( lexer.getBuffer() +
+								" Illegal character in hostname:" + lexer.lookAhead(0),
 								lexer.getPtr() );
 					}
 				}

src/gov/nist/core/LexerCore.java
@@ -338,7 +338,7 @@ public class LexerCore extends StringTokenizer {
 	}
 
 	public String ttoken() {
-		StringBuffer nextId = new StringBuffer();
+		int startIdx = ptr;
 		try {
 			while (hasMoreChars()) {
 				char nextChar = lookAhead(0);
@@ -356,19 +356,18 @@ public class LexerCore extends StringTokenizer {
 					|| nextChar == '.'
 					|| nextChar == '*') {
 					consume(1);
-					nextId.append(nextChar);
 				} else
 					break;
 
 			}
-			return nextId.toString();
+			return buffer.substring(startIdx, ptr);
 		} catch (ParseException ex) {
-			return nextId.toString();
+			return null;
 		}
 	}
 
 	public String ttokenAllowSpace() {
-		StringBuffer nextId = new StringBuffer();
+		int startIdx = ptr;
 		try {
 			while (hasMoreChars()) {
 				char nextChar = lookAhead(0);
@@ -386,20 +385,19 @@ public class LexerCore extends StringTokenizer {
 					|| nextChar == ' '
 					|| nextChar == '\t'
 					|| nextChar == '*') {
-					nextId.append(nextChar);
 					consume(1);
 				} else
 					break;
 
 			}
-			return nextId.toString();
+			return buffer.substring(startIdx, ptr);
 		} catch (ParseException ex) {
-			return nextId.toString();
+			return null;
 		}
 	}
 
 	public String ttokenSafe() {
-		StringBuffer nextId = new StringBuffer();
+		int startIdx = ptr;
 		try {
 			while (hasMoreChars()) {
 				char nextChar = lookAhead(0);
@@ -429,15 +427,14 @@ public class LexerCore extends StringTokenizer {
 					|| nextChar == '?'
 					|| nextChar == '\"'
 					|| nextChar == '*' ) {
-					nextId.append(nextChar);
 					consume(1);
 				} else
 					break;
 
 			}
-			return nextId.toString();
+			return buffer.substring(startIdx, ptr); 
 		} catch (ParseException ex) {
-			return nextId.toString();
+			return null;
 		}
 	}
 

src/gov/nist/javax/sdp/parser/MediaFieldParser.java
@@ -32,7 +32,7 @@ import java.util.*;
 /**
  * Parser for Media field.
  *
- * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.4 $ $Date: 2006-07-13 09:02:42 $
+ * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.5 $ $Date: 2007-02-06 16:40:00 $
  *
  * @author  Olivier Deruelle 
  * @author M. Ranganathan   <br/>
@@ -101,7 +101,8 @@ public class MediaFieldParser extends SDPParser {
 			// The formats list:
 			Vector formatList = new Vector();
 			while (lexer.hasMoreChars()) {
-				if (lexer.lookAhead(0) == '\n' || lexer.lookAhead(0) == '\r')
+				char la = lexer.lookAhead(0);
+				if (la == '\n' || la == '\r')
 					break;
 				this.lexer.SPorHT();
 				//while(lexer.lookAhead(0) == ' ') lexer.consume(1);
@@ -156,6 +157,29 @@ public class MediaFieldParser extends SDPParser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2006/07/13 09:02:42  mranga
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  jeroen van bemmel
+ * Reviewed by:   mranga
+ * Moved some changes from jain-sip-1.2 to java.net
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  * Revision 1.3  2006/06/19 06:47:26  mranga
  * javadoc fixups
  *

src/gov/nist/javax/sip/parser/AddressParametersParser.java
@@ -32,7 +32,7 @@ import java.text.ParseException;
 /**
  * Address parameters parser.
  *
- * @version 1.2 $Revision: 1.7 $ $Date: 2006-07-13 09:02:07 $
+ * @version 1.2 $Revision: 1.8 $ $Date: 2007-02-06 16:40:02 $
  * @author M. Ranganathan 
  * @since 1.1  
  * 
@@ -55,14 +55,15 @@ public class AddressParametersParser extends ParametersParser {
 			AddressImpl addr = addressParser.address();
 			addressParametersHeader.setAddress(addr);
 			lexer.SPorHT();
+			char la = this.lexer.lookAhead(0);
 			if ( this.lexer.hasMoreChars() &&
-			     this.lexer.lookAhead(0) != '\0' &&
-			     this.lexer.lookAhead(0) != '\n' &&
+			     la != '\0' &&
+			     la != '\n' &&
 			     this.lexer.startsId()) {
 
 			     super.parseNameValueList(addressParametersHeader);
-			      
-				
+
+
 			}  else super.parse(addressParametersHeader);
 		
 		} catch (ParseException ex) {

src/gov/nist/javax/sip/parser/AddressParser.java
@@ -30,7 +30,7 @@ import java.text.ParseException;
 
 /** Parser for addresses.
  *
- * @version 1.2 $Revision: 1.7 $ $Date: 2006-07-13 09:01:57 $
+ * @version 1.2 $Revision: 1.8 $ $Date: 2007-02-06 16:40:02 $
  * @author M. Ranganathan   
  * 
  *
@@ -96,22 +96,21 @@ public class AddressParser extends Parser {
 		try {
 			int k = 0;
 			while (lexer.hasMoreChars()) {
-				if (lexer.lookAhead(k) == '<'
-					|| lexer.lookAhead(k) == '\"'
-					|| lexer.lookAhead(k) == ':'
-					|| lexer.lookAhead(k) == '/')
+				char la = lexer.lookAhead(k);
+				if (la == '<'
+					|| la == '\"'
+					|| la == ':'
+					|| la == '/')
 					break;
-				else if (lexer.lookAhead(k) == '\0')
+				else if (la == '\0')
 					throw createParseException("unexpected EOL");
 				else
 					k++;
 			}
-			if (this.lexer.lookAhead(k) == '<'
-				|| this.lexer.lookAhead(k) == '\"') {
+			char la = lexer.lookAhead(k);
+			if (la == '<' || la == '\"') {
 				retval = nameAddr();
-			} else if (
-				this.lexer.lookAhead(k) == ':'
-					|| this.lexer.lookAhead(k) == '/') {
+			} else if (la == ':' || la == '/') {
 				retval = new AddressImpl();
 				URLParser uriParser = new URLParser((Lexer) lexer);
 				GenericURI uri = uriParser.uriReference();
@@ -134,6 +133,29 @@ public class AddressParser extends Parser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.7  2006/07/13 09:01:57  mranga
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  jeroen van bemmel
+ * Reviewed by:   mranga
+ * Moved some changes from jain-sip-1.2 to java.net
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  * Revision 1.4  2006/06/19 06:47:27  mranga
  * javadoc fixups
  *

src/gov/nist/javax/sip/parser/ChallengeParser.java
@@ -32,7 +32,7 @@ import java.text.ParseException;
 /**
  * Parser for the challenge portion of the authentication header.
  *
- * @version 1.2 $Revision: 1.6 $ $Date: 2006-07-13 09:02:15 $
+ * @version 1.2 $Revision: 1.7 $ $Date: 2007-02-06 16:40:02 $
  * @since 1.1
  *
  * @author Olivier Deruelle    <br/>
@@ -96,7 +96,8 @@ public abstract class ChallengeParser extends HeaderParser {
 			while (lexer.lookAhead(0) != '\n') {
 				this.parseParameter(header);
 				this.lexer.SPorHT();
-				if (lexer.lookAhead(0) == '\n' || lexer.lookAhead(0) == '\0')
+				char la = lexer.lookAhead(0);
+				if (la == '\n' || la == '\0')
 					break;
 				this.lexer.match(',');
 				this.lexer.SPorHT();

src/gov/nist/javax/sip/parser/ContactParser.java
@@ -25,17 +25,19 @@ package gov.nist.javax.sip.parser;
 
 import gov.nist.javax.sip.address.AddressImpl;
 import gov.nist.javax.sip.address.SipUri;
-import gov.nist.javax.sip.header.*;
+import gov.nist.javax.sip.header.Contact;
+import gov.nist.javax.sip.header.ContactList;
+import gov.nist.javax.sip.header.SIPHeader;
+
+import javax.sip.address.URI;
 import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Iterator;
 
-import javax.sip.address.URI;
-
 /**
  * A parser for The SIP contact header.
  * 
- * @version 1.2 $Revision: 1.10 $ $Date: 2007-01-19 18:53:18 $
+ * @version 1.2 $Revision: 1.11 $ $Date: 2007-02-06 16:40:02 $
  * @since 1.1
  */
 public class ContactParser extends AddressParametersParser {
@@ -91,10 +93,11 @@ public class ContactParser extends AddressParametersParser {
 			}
 			retval.add(contact);
 			this.lexer.SPorHT();
-			if (lexer.lookAhead(0) == ',') {
+			char la = lexer.lookAhead(0);
+			if (la == ',') {
 				this.lexer.match(',');
 				this.lexer.SPorHT();
-			} else if (lexer.lookAhead(0) == '\n' || lexer.lookAhead(0) == '\0')
+			} else if (la == '\n' || la == '\0')
 				break;
 			else
 				throw createParseException("unexpected char");

commit 5667c301a2d6f417a8e36cd067aab2fc20018725

Performance improvement for StringTokenizer.lookAhead(int)
Reverted bad optimization attempt via exception-driven branching introduced in 2a8aa8a162713361ef862afe0515de90392dfbe4.

Rationale:
Profiling shows a huge amount of CPU time wasted in this method in the constructor of the ArrayIndexOutOfBoundsException, which is only used for local branch control and is otherwise unnecessary. Creating the exception involves generating a stack trace; even if the probability of hitting an out-of-bounds lookAhead is low, doing this a few times is much more costly than what we save by avoiding the addition and comparison combo in the in-bounds cases. Note that exception handling introduces branching in the method anyway.

src/gov/nist/core/StringTokenizer.java
@@ -134,12 +134,11 @@ public class StringTokenizer {
 
     public char lookAhead(int k) throws ParseException {
         // Debug.out.println("ptr = " + ptr);
-        try {
+    	assert k >= 0;
+    	if (buffer.length - ptr > k)
             return buffer[ptr + k];
-        }
-        catch (IndexOutOfBoundsException e) {
+    	else
             return '\0';
-        }
     }
 
     public char getNextChar() throws ParseException {